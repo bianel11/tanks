c8a320db28ae67f95002274cfe976750ec7bd376
Bianel <bianel9815@gmail.com>
2022-07-09 21:07:42 -0400

    finish tutorial

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -1,25 +1,25 @@
 ﻿using UnityEngine;
 using System.Collections;
-//using UnityEngine.SceneManagement;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
-    public int m_NumRoundsToWin = 5;        
-    public float m_StartDelay = 3f;         
-    public float m_EndDelay = 3f;           
-    public CameraControl m_CameraControl;   
-    public Text m_MessageText;              
-    public GameObject m_TankPrefab;         
-    public TankManager[] m_Tanks;           
+    public int m_NumRoundsToWin = 5; //Número de rondas que un jugador debe ganar para ganar el juego 
+    public float m_StartDelay = 3f; ///Delay entre las fases de RoundStarting yRoundPlaying
+    public float m_EndDelay = 3f; ///Delay entre las fases de RoundPlaying y RoundEnding
+    public CameraControl m_CameraControl; ///Referencia al sccript de CameraControl 
+    public Text m_MessageText; //Referencia al texto para mostrar mensajes
+    public GameObject m_TankPrefab;
+    public TankManager[] m_Tanks;
 
 
-    private int m_RoundNumber;              
-    private WaitForSeconds m_StartWait;     
-    private WaitForSeconds m_EndWait;       
-/*    private TankManager m_RoundWinner;
-    private TankManager m_GameWinner;       
-*/
+    private int m_RoundNumber;
+    private WaitForSeconds m_StartWait;
+    private WaitForSeconds m_EndWait;
+    private TankManager m_RoundWinner;
+    private TankManager m_GameWinner;
+
 
     const float k_MaxDepenetrationVelocity = float.PositiveInfinity;
 
@@ -27,7 +27,7 @@ public class GameManager : MonoBehaviour
     {
         // This line fixes a change to the physics engine.
         Physics.defaultMaxDepenetrationVelocity = k_MaxDepenetrationVelocity;
-        
+
         m_StartWait = new WaitForSeconds(m_StartDelay);
         m_EndWait = new WaitForSeconds(m_EndDelay);
 
@@ -69,7 +69,7 @@ public class GameManager : MonoBehaviour
         yield return StartCoroutine(RoundPlaying());
         yield return StartCoroutine(RoundEnding());
 
-/*        if (m_GameWinner != null)
+        if (m_GameWinner != null)
         {
             SceneManager.LoadScene(0);
         }
@@ -77,23 +77,58 @@ public class GameManager : MonoBehaviour
         {
             StartCoroutine(GameLoop());
         }
-*/    }
+
+    }
 
 
     private IEnumerator RoundStarting()
     {
+        ResetAllTanks();
+        DisableTankControl();
+
+        m_CameraControl.SetStartPositionAndSize();
+
+        m_RoundNumber++;
+        m_MessageText.text = "ROUND" + m_RoundNumber;
+
+
         yield return m_StartWait;
     }
 
 
     private IEnumerator RoundPlaying()
     {
-        yield return null;
+        // Cuando empiece la ronda dejo que los tanques se muevan.
+
+        EnableTankControl();
+        // Borro el texto de la pantalla
+        m_MessageText.text = string.Empty;
+        // Mientras haya más de un tanque...
+
+        while (!OneTankLeft())
+        {
+            // ... vuelvo al frame siguiente.
+            yield return null;
+        }
     }
 
 
     private IEnumerator RoundEnding()
     {
+        DisableTankControl();
+        m_RoundWinner = null;
+
+        m_RoundWinner = GetRoundWinner();
+
+        if (m_RoundWinner != null)
+        {
+            m_RoundWinner.m_Wins++;
+        }
+
+        m_GameWinner = GetGameWinner();
+        string message = EndMessage();
+        m_MessageText.text = message;
+
         yield return m_EndWait;
     }
 
@@ -111,7 +146,7 @@ public class GameManager : MonoBehaviour
         return numTanksLeft <= 1;
     }
 
-/*
+
     private TankManager GetRoundWinner()
     {
         for (int i = 0; i < m_Tanks.Length; i++)
@@ -155,7 +190,7 @@ public class GameManager : MonoBehaviour
 
         return message;
     }
-*/
+
 
     private void ResetAllTanks()
     {
--- Assets/Scripts/Managers/TankManager.cs
+++ Assets/Scripts/Managers/TankManager.cs
@@ -1,33 +1,40 @@
 ﻿using System;
 using UnityEngine;
 
-[Serializable]
+[Serializable] //Hace que los atributos aparezcan en el inspector (si no los escondemos)
 public class TankManager
 {
-    public Color m_PlayerColor;            
-    public Transform m_SpawnPoint;         
-    [HideInInspector] public int m_PlayerNumber;             
-    [HideInInspector] public string m_ColoredPlayerText;
-    [HideInInspector] public GameObject m_Instance;          
-    [HideInInspector] public int m_Wins;                     
+    //Esta clase gestiona la configuración del tanque junto con el GameManager
+    //gestiona el comportamiento de los tanques y si los jugadores tienen cont
+    //  rol sobre el tanque
+    //en los distintos momentos del juego
+    public Color m_PlayerColor;
+    public Transform m_SpawnPoint; //Posición y direción en la que se generará el tanque
+    [HideInInspector] public int m_PlayerNumber; //Especifica con qué jugador está actuando el Game Manager 
+    [HideInInspector] public string m_ColoredPlayerText; ///String que reprsenta el color del tanque
+    [HideInInspector] public GameObject m_Instance; //Refernecia a la instancia del tanque cuando se crea
+    [HideInInspector] public int m_Wins; ///Número de victorias del jugador 
 
 
-    private TankMovement m_Movement;       
-    private TankShooting m_Shooting;
-    private GameObject m_CanvasGameObject;
+    private TankMovement m_Movement; ///Referencia al script de movimiento deltanque. Utilizado para deshabilitar y habilitar el control
+    private TankShooting m_Shooting; //Referencia al script de disparo del tanque. Utilizado para deshabilitar y habilitar el contro
+    private GameObject m_CanvasGameObject; //Utilizado para deshabilitar el UI del mundo durante als fases de inicio y fin de cada ronda
 
 
     public void Setup()
     {
+        //Cojo referencias de los componentes
         m_Movement = m_Instance.GetComponent<TankMovement>();
         m_Shooting = m_Instance.GetComponent<TankShooting>();
         m_CanvasGameObject = m_Instance.GetComponentInChildren<Canvas>().gameObject;
+        // /Ajusto los número de jugadores para que sean iguales en todos los scripts
 
         m_Movement.m_PlayerNumber = m_PlayerNumber;
         m_Shooting.m_PlayerNumber = m_PlayerNumber;
 
+        ///Creo un string usando el color del tanque que diga PLAYER 1, etc.
         m_ColoredPlayerText = "<color=#" + ColorUtility.ToHtmlStringRGB(m_PlayerColor) + ">PLAYER " + m_PlayerNumber + "</color>";
-
+        //Cojo todos los renderers del tanque
         MeshRenderer[] renderers = m_Instance.GetComponentsInChildren<MeshRenderer>();
 
         for (int i = 0; i < renderers.Length; i++)
@@ -36,7 +43,7 @@ public class TankManager
         }
     }
 
-
+    ///Usado durante la fases del juego en las que el jugador no debe poder controlar el tanque
     public void DisableControl()
     {
         m_Movement.enabled = false;
@@ -45,7 +52,7 @@ public class TankManager
         m_CanvasGameObject.SetActive(false);
     }
 
-
+    //Usado durante la fases del juego en las que el jugador debe poder controlar el tanque
     public void EnableControl()
     {
         m_Movement.enabled = true;
@@ -54,7 +61,7 @@ public class TankManager
         m_CanvasGameObject.SetActive(true);
     }
 
-
+    ///Usado al inicio de cada ronda para poner el tanque en su estado inicial
     public void Reset()
     {
         m_Instance.transform.position = m_SpawnPoint.position;
